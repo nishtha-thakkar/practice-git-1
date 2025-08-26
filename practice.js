712ed1aa3143edd8d53c1ac53ebb1a8b7ed87c7a
nishtha-thakkar <nishthathakkar3@gmail.com>
2025-08-26 22:32:30 +0530

    Add files via upload
    both question completed , kindly check

---
+++ practice.js
@@ -0,0 +1,25 @@
+// Q1. Print "Your Name" in the console   in next line "Print your Strength".
+
+let fullName = "Nishtha"
+console.log(fullName);
+
+let strength = "my strength is i am very quick lerner and also my strongest strength is  my parents";
+console.log(strength);
+
+
+// Q2. Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let myName = "Nishtha"
+console.log("myName is",myName);
+
+let fatherName = "Mahendrabhai"
+console.log("father's Name",fatherName);
+
+let motherName = "pannaben"
+console.log("mother's Name",motherName);
+
